448e1d20101a155703ec11030b6af45aeeb85265
Chris <limadog2003@gmail.com>
2017-03-13 09:58:42 -0400

    Initial commit for master branch

---
+++ Item_list.java
@@ -0,0 +1,24 @@
+package edu.bu.met.cs566.Knapsack_Problems;
+
+import java.util.ArrayList;
+
+public class Item_list 
+	{
+	private int totalValue;
+	private ArrayList<Items> List;
+	
+	public Item_list ()
+		{
+		
+		}
+	public int getValue()
+		{
+		return totalValue;
+		}
+	
+	public ArrayList<Items> getList ()
+		{
+		return List;
+		}
+	
+	}
---
+++ Items.java
@@ -0,0 +1,57 @@
+package edu.bu.met.cs566.Knapsack_Problems;
+
+public class Items 
+	{
+	private int value, weight, num;
+	
+	public Items (int num, int value, int weight)
+		{
+		this.num = num;
+		this.value = value;
+		this.weight = weight;
+		}
+
+	/**
+	 * @return the value
+	 */
+	public int getValue() {
+		return value;
+	}
+
+	/**
+	 * @param value the value to set
+	 */
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	/**
+	 * @return the weight
+	 */
+	public int getWeight() {
+		return weight;
+	}
+
+	/**
+	 * @param weight the weight to set
+	 */
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	/**
+	 * @return the num
+	 */
+	public int getNum() {
+		return num;
+	}
+
+	/**
+	 * @param num the num to set
+	 */
+	public void setNum(int num) {
+		this.num = num;
+	}
+	
+	
+	}
